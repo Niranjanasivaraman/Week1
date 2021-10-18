ad046be589b9cf943f5c4d1e0f139bd56532caf7
Niranjanasivaraman <91813295+Niranjanasivaraman@users.noreply.github.com>
2021-10-18 22:21:24 +0530

    Add files via upload
    Even numbers program

---
+++ EvenNumbers.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+for (int i = 0; i<=50; i++) {
+	if(i%2==0){   //In this line instead of (i%2==0) use (i%2!=0) for odd numbers
+		System.out.println(i + "is an even number");
+	}
+	
+}
+	}
+
+}
